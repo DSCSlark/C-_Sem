36b71d773125a7432375b4d4cda86c16277d8f13
DSCSlark <9047915555@mail.ru>
2023-03-21 14:38:56 +0300

    task 2_0 rdy

---
+++ 2_sem/2_0/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает трехзначное число и на выходе показывает последнюю цифру этого числа.
+
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    return num % 10;
+}
+
+// 123 % 10 -> 3
+// 123 // 10 -> 12
+
+int th_num = new Random().Next(100, 1000);
+int result = TakeNum(th_num);
+Console.WriteLine(result);
