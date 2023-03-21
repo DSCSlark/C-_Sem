573493292e733d1ec320a182e9026a6846d641d8
DSCSlark <9047915555@mail.ru>
2023-03-21 15:14:04 +0300

    task 2_1 rdy

--- 2_sem/2_1/Program.cs
+++ 2_sem/2_1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру из числа
+
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
+
+int Num(int num)
+{
+    Console.WriteLine(num);
+    int num1 = num / 10;
+    int num2 = num % 10;
+    if (num1 > num2) return num1;
+    return num2;
+}
+int a = Num(new Random().Next(10, 100));
+Console.WriteLine(a);
