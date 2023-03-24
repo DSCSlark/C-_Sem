9fbbcece2b74f4bf9bc3c45f06af18be26a0427e
DSCSlark <9047915555@mail.ru>
2023-03-24 15:11:43 +0300

    task 3_0 done

---
+++ 3_sem/3_0/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на вход принимает координаты точки (X и Y), при чем Х ≠ 0 или У ≠ 0 и выдает номер четверти в плоскости, в которой находится эта точка.
+// Если "х" движется в правую сторону, это +
+// Если "х" движется в левую сторону, это -
+// Если "у" движется вверх, это +
+// Если "у" движется вниз, это -
+
+void Quarters(int x, int y)
+{
+    if (x == 0 || y == 0)
+        Console.WriteLine("x = 0, y = 0");
+    else if (x > 0 && y > 0)
+        Console.WriteLine("I");
+    else if (x < 0 && y > 0)
+        Console.WriteLine("II");
+    else if (x < 0 && y < 0)
+        Console.WriteLine("III");
+    else if (x > 0 && y < 0)
+        Console.WriteLine("IV");
+}
+
+int num_1 = int.Parse(Console.ReadLine()!);
+int num_2 = int.Parse(Console.ReadLine()!);
+Quarters(num_1, num_2);
