1c056ae0c12c133c56ff9f85a5b9a53059bea467
DSCSlark <9047915555@mail.ru>
2023-04-06 13:41:50 +0300

    task 4_0 done

---
+++ 4_sem/4_0/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 1. Напишите программу, которая на вход принимает число (А) и выдает сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36 
+
+int SumNum(int num)
+{
+    int all_sum = 0;
+
+    for(int i = 0; i <= num; i++)
+        all_sum += i;
+
+    return all_sum;
+}
+
+int val = int.Parse(Console.ReadLine()!);
+int result = SumNum(val);
+Console.WriteLine(result);
