e1f86a93bd997a36b4e4c8ae983b0d86d651bfc2
DSCSlark <9047915555@mail.ru>
2023-04-06 14:46:05 +0300

    task 4_2 done

---
+++ 4_sem/4_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// 2. Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+int ymn(int num)
+{
+    int x = 1;
+    for(int i = 1; i <= num; i++)
+    {
+        x *= i;
+    }
+    return x;
+}
+
+Console.WriteLine("Write a number: ");
+int P = ymn(int.Parse(Console.ReadLine()!));
+Console.WriteLine(P);
