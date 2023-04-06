bfafbe2fbb97f82f81a22c25c69994411b2548bb
DSCSlark <9047915555@mail.ru>
2023-04-06 14:30:53 +0300

    task 4_1 done

---
+++ 4_sem/4_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает число и выдаетколичество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int NumCount(int num)
+{
+    int count = 0;
+    for(int i = 1; num > 0; i++)
+    {
+        num /= 10;
+        count = i;
+    }
+    return count;
+}
+
+Console.WriteLine("Enter a number: ");
+int a = int.Parse(Console.ReadLine()!);
+int result = NumCount(a);
+Console.WriteLine(result);
