755424d84960dc6313ac9562c09114596cb8edaa
DSCSlark <9047915555@mail.ru>
2023-04-06 15:01:08 +0300

    task 4_3 done

---
+++ 4_sem/4_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая будет создавать массив из 8 элементов, заполенный нулями и единицами в случайном порядке.
+
+void binArr(int num)
+{
+    int[] arr = new int[num];
+    for(int i = 0; i < num; i++)
+    {
+        arr[i] = new Random().Next(2);
+        Console.Write($"{arr[i]}, ");
+    }
+    //Console.WriteLine("[{0}]", string.Join(", ", arr));
+}
+Console.WriteLine("Введите длину массива: ");
+int len = int.Parse(Console.ReadLine()!);
+binArr(len);
