3e914c02134ca37e0cfdfdb2be775883863719aa
DSCSlark <9047915555@mail.ru>
2023-04-12 17:17:21 +0300

    task 5_1 done

---
+++ 5_sem/5_1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу замены элементов массива: 
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+// Пример: [-4, -8, 8, 2] -> [4, 8, -8, -2].
+
+int[] RandArr()
+{
+Console.Write("Enter array length: ");
+int len = int.Parse(Console.ReadLine()!);
+Console.Write("Enter minimal range value: ");
+int min = int.Parse(Console.ReadLine()!);
+Console.Write("Enter maximum range value: ");
+int max = int.Parse(Console.ReadLine()!);
+
+int[] arr = new int[len];
+for (int i = 0; i < len; i++)
+{
+arr[i] = new Random().Next(min, max + 1);
+}
+return arr;
+}
+
+void PrintArr(int[] arr)
+{
+Console.WriteLine();
+for (int i = 0; i < arr.Length; i++)
+{
+Console.Write($"{arr[i]} ");
+
+}
+Console.WriteLine();
+// Console.WriteLine("[{0}]", string.Join(", ", arr));
+}
+
+void reverse(int[] arr)
+{
+for(int i = 0; i < arr.Length; i++)
+{
+arr[i] *= -1;
+}
+}
+
+int[] array = RandArr();
+PrintArr(array);
+reverse(array);
+PrintArr(array);
