340c495ea7a4449860237955f8e0720218fef7b8
DSCSlark <9047915555@mail.ru>
2023-04-20 17:45:03 +0300

    task 6_1 done

---
+++ 6_sem/6_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 1. Напишите программу, которая на вход принимает три числа и проверяет, может ли существовать треугольник со сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+
+void Treugolnik(int a, int b, int c)
+{
+    if(a < b + c && b < a + c && c < a + b)
+        Console.WriteLine("Yes");
+    else
+        Console.WriteLine("No");
+}
+
+Treugolnik(a, b, c);
