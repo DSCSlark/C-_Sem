4bc47414b26e6a503e0fb1143b9f857019be9911
DSCSlark <9047915555@mail.ru>
2023-04-20 17:53:36 +0300

    task 6_2 done

--- 6_sem/6_2/Program.cs
+++ 6_sem/6_2/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿// Задача 2. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+string binary(int num)
+{
+    string result = "";
+    while(num > 0)
+    {
+        result = num % 2 + result;
+        num /= 2;
+    }
+    return result;
+}
+
+Console.Write("Enter a number: ");
+int a = int.Parse(Console.ReadLine()!);
+string answer = binary(a);
+Console.WriteLine(answer);
